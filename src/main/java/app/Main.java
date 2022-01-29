7431fc29636a81a4e0a3ea4a1217c03e6a0000cf
Erchenko Igor <erchenkoia@msk.bcs.ru>
2022-01-29 20:28:04 +0300

    Branch B commit 2

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,10 +2,6 @@ package app;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("BranchA2");
-    System.out.println("BranchA2.1");
-    System.out.println("BranchA2.2");
-    System.out.println("BranchB1");
-    System.out.println("BranchC1");
+    System.out.println("BranchA4");
   }
 }
